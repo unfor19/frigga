34421910e5a386914ffa5cad9eeb651ad9f321bb
Meir Gabay <meir@prodops.io>
2020-06-26 23:21:33 +0300

    add comments and dump to file

--- scripts/frigga.py
+++ scripts/frigga.py
@@ -1,3 +1,4 @@
+import json
 import click
 from .grafana import get_metrics_list
 from .config import print_json
@@ -39,6 +40,7 @@ def cli(ci):
 @cli.command()
 @click.option('--grafana-url', '-gurl', prompt=True)
 @click.option('--grafana-api-key', '-gkey', prompt=True, hide_input=True)
+# @click.option('--file', '-f', prompt=False, default=".", help="Output metrics.json file to pwd")
 def metrics_get(grafana_url, grafana_api_key):
     """Provide Grafana UrL and Grafana API Key (Viewer)\n
 Returns a list of metrics that are used in all dashboards"""
@@ -46,3 +48,5 @@ Returns a list of metrics that are used in all dashboards"""
         raise Exception("Must contain 'http' or 'https'")
     metrics = get_metrics_list(grafana_url, grafana_api_key)
     print_json(metrics)
+    with open('.metrics.json', 'w') as file:
+        json.dump(metrics, file, indent=2, sort_keys=True)
--- scripts/grafana.py
+++ scripts/grafana.py
@@ -15,6 +15,8 @@ def grafana_http_request(path, api_key, base_url="http://localhost:3000"):
 
 
 def get_metrics_from_expr(expression, ignored_words_list):
+
+    # Remove all [] and {}
     regex_patterns = [
         r"\{(.+?)\}",
         r"\[(.+?)\]",
@@ -30,6 +32,7 @@ def get_metrics_from_expr(expression, ignored_words_list):
             for result in results:
                 expression = expression.replace(result, " ")
 
+    # Remove all the ignored_words
     for item in ignored_words_list:
         pattern = r"[ \(\)\{\}]" + item + r"[ \(\)\{\}]"
         results = re.findall(pattern, expression)
@@ -41,18 +44,25 @@ def get_metrics_from_expr(expression, ignored_words_list):
             ]
             for result in results:
                 expression = expression.replace(result, " ")
+
+    # Remove leftovers
+    # TODO: Avoid this loop, cleanup in previous step
+    math_signs = ["*", "-", "+", "^", "/", "]", "[", "{", "}"]
+    for sign in math_signs:
+        expression = expression.replace(sign, " ")
     metrics = expression.split()
     return metrics
 
 
 def get_metrics_list(base_url, api_key):
-    metrics = []
     ignored_words = get_ignored_words()
+    print(ignored_words)
     dashboards = grafana_http_request(
         "/api/search?query=",
         api_key,
         base_url
     ).json()
+    metrics = dict()
     for dashboard in dashboards:
         dashboard_body = grafana_http_request(
             f"/api/dashboards/uid/{dashboard['uid']}",
@@ -60,6 +70,7 @@ def get_metrics_list(base_url, api_key):
             base_url
         ).json()
         expressions = scrape_value_by_key(dashboard_body, "expr")
+        dashboard_metrics = []
         for expression in expressions:
             expr_metrics = get_metrics_from_expr(expression, ignored_words)
             if expr_metrics:
@@ -68,7 +79,8 @@ def get_metrics_list(base_url, api_key):
                         float(metric)
                     except:  # noqa: 722
                         if len(metric) > 5 and "/" not in metric:
-                            metrics.append(metric)
-        metrics = list(set(metrics))
-        metrics.sort()
+                            dashboard_metrics.append(metric)
+        dashboard_metrics = list(set(dashboard_metrics))
+        dashboard_metrics.sort()
+        metrics[dashboard_body['meta']['slug']] = dashboard_metrics
     return metrics
