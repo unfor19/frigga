d6362b41a2c81764b3202cac5caff1af5bda6de9
Meir Gabay <meir@prodops.io>
2020-06-26 18:10:02 +0300

    shorter cmd

--- scripts/frigga.py
+++ scripts/frigga.py
@@ -3,7 +3,33 @@ from .grafana import get_metrics_list
 from .config import print_json
 
 
-@click.group()
+class AliasedGroup(click.Group):
+    def get_command(self, ctx, cmd_name):
+        aliases = {
+            "m": "metrics",
+            "a": "apply",
+            "g": "get",
+            "d": "delete",
+            "l": "list",
+        }
+        if len(cmd_name) == 2:
+            words = [aliases[char] for char in cmd_name if char in aliases]
+            cmd_name = "-".join(words)
+
+        rv = click.Group.get_command(self, ctx, cmd_name)
+        if rv is not None:
+            return rv
+        matches = [x for x in self.list_commands(ctx)
+                   if x.startswith(cmd_name)]
+        if not matches:
+            return None
+        elif len(matches) == 1:
+            return click.Group.get_command(self, ctx, matches[0])
+        ctx.fail(f"Too many matches: {', '.join(sorted(matches))}")
+
+
+# @click.group()
+@ click.command(cls=AliasedGroup)
 @click.option('--ci', '-ci', is_flag=True, help="Use this flag to avoid deletion confirmation prompts")  # noqa: E501
 def cli(ci):
     """Test"""  # noqa: E501
@@ -13,7 +39,7 @@ def cli(ci):
 @cli.command()
 @click.option('--grafana-url', '-gurl', prompt=True)
 @click.option('--grafana-api-key', '-gkey', prompt=True, hide_input=True)
-def get_metrics(grafana_url, grafana_api_key):
+def metrics_get(grafana_url, grafana_api_key):
     """Provide Grafana UrL and Grafana API Key (Viewer)\n
 Returns a list of metrics that are used in all dashboards"""
     if "http" not in grafana_url:
