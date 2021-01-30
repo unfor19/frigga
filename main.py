bd6695c93a342bcbabc1be4154e733f3589aea23
Meir Gabay <unfor19@gmail.com>
2021-01-30 13:49:00 +0200

    restructure

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from scripts import frigga
+import runpy
+
+
+def main():
+    app_package = runpy.run_module(
+        mod_name="src.frigga", init_globals=globals())
+    app_package['main']()
+
 
 if __name__ == "__main__":
-    frigga.cli()
+    main()
---
+++ src/frigga/__main__.py
@@ -0,0 +1,9 @@
+from .frigga import cli as frigga_cli
+
+
+def main():
+    frigga_cli()
+
+
+if __name__ == "__main__":
+    main()
