6a11190be553338767424720472c1f232e009532
Meir Gabay <unfor19@gmail.com>
2021-02-07 03:10:02 +0200

    curl silent

--- kubernetes/apply_changes.sh
+++ kubernetes/apply_changes.sh
@@ -18,7 +18,7 @@ cd "$FRIGGA_FOLDER"
 
 GRAFANA_HOST="http://grafana.default.svc.cluster.local:3000"
 RANDOM_KEY_NAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 10 ; echo '')
-GRAFANA_API_KEY=$(curl -X POST -sL --user admin:admin -H "Content-Type: application/json" --data '{"name":"'${RANDOM_KEY_NAME}'","role":"Viewer","secondsToLive":86400}' ${GRAFANA_HOST}/api/auth/keys | jq -r .key)
+GRAFANA_API_KEY=$(curl -s -X POST -sL --user admin:admin -H "Content-Type: application/json" --data '{"name":"'${RANDOM_KEY_NAME}'","role":"Viewer","secondsToLive":86400}' ${GRAFANA_HOST}/api/auth/keys | jq -r .key)
 
 # Install frigga
 python3 -m pip install --upgrade pip
