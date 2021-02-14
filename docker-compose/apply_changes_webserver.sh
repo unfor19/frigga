e061905272167eba80139cd85b103213b1ff2257
Meir Gabay <unfor19@gmail.com>
2021-02-14 23:45:30 +0200

    split env vars

--- docker-compose/apply_changes_webserver.sh
+++ docker-compose/apply_changes_webserver.sh
@@ -18,9 +18,9 @@ FRIGGA_URL=http://localhost:8083
 PROM_URL=http://prometheus:9090
 PROM_YAML_PATH=/etc/prometheus/prometheus.yml
 GRAFANA_URL=http://grafana:3000
-GRAFANA_API_KEY=$GRAFANA_API_KEY
 SLEEP_SECONDS=15
 " > .env.webserver.ci
+echo "GRAFANA_API_KEY=$GRAFANA_API_KEY" >> .env.webserver.ci
 _DOCKER_TAG="${DOCKER_TAG:-"unfor19/frigga:latest"}"
 
 docker run --rm -t --network host --env-file .env.webserver.ci "$_DOCKER_TAG" client-run
