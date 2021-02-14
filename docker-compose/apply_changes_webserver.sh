e4fb9cc12164c4cf3cc904f3031f2c5b049c3d40
Meir Gabay <unfor19@gmail.com>
2021-02-14 22:53:55 +0200

    added test for webserver

---
+++ docker-compose/apply_changes_webserver.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+export GRAFANA_API_KEY="$(cat .apikey || true)"
+
+error_msg(){
+    msg="$1"
+    echo ">> [ERROR] ${msg}"
+    exit 1
+}
+
+
+[[ -z "${GRAFANA_API_KEY}" ]] && error_msg ".apikey file is empty"
+
+export \
+    FRIGGA_URL="http://localhost:8083" \
+    PROM_URL="http://prometheus:9090" \
+    PROM_YAML_PATH="/etc/prometheus/prometheus.yml"
+    GRAFANA_URL="http://grafana:3000" \
+    SLEEP_SECONDS=10
+
+frigga client-run
--- docker-compose/deploy_stack.sh
+++ docker-compose/deploy_stack.sh
@@ -29,7 +29,11 @@ grafana_update_admin_password(){
 network=$(docker network ls | grep frigga_net || true)
 [[ -n "$network" ]] && echo "ERROR: wait for network to be deleted, docker network ls, or restart docker daemon" && exit
 cp docker-compose/prometheus-original.yml docker-compose/prometheus.yml
-docker-compose --project-name frigga \
+
+_DOCKER_TAG="${DOCKER_TAG:-"unfor19/frigga:latest"}"
+echo "DOCKER_TAG=${_DOCKER_TAG}" > .env.ci
+
+docker-compose --project-name frigga --env-file .env.ci \
     --file docker-compose/docker-compose.yml \
     up --detach
 
