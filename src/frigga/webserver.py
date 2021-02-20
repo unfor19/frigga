c6d90442c188862c4f0c982e616cb31e8278efb2
Meir Gabay <unfor19@gmail.com>
2021-02-20 15:42:58 +0200

    added debug, logging and render template

--- src/frigga/webserver.py
+++ src/frigga/webserver.py
@@ -1,6 +1,6 @@
 import logging
 
-from flask import Flask, request, jsonify, abort
+from flask import Flask, request, jsonify, abort, render_template
 from waitress import serve
 
 from .prometheus import reload_prom as prometheus_reload
@@ -10,15 +10,26 @@ from .grafana import get_metrics_list as grafana_list
 
 
 app = Flask(__name__)
+logging.basicConfig()
+logger = logging.getLogger('waitress')
+logger.setLevel(logging.INFO)
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
+    logger.error(e)
     return jsonify(error=str(e)), 500
+
+
+@app.route('/', methods=['GET', ])
+def root():
+    logger.info(request)
+    return render_template('index.html')
 
 
 @app.route('/grafana/list', methods=['POST', ])
 def list_grafana_metrics():
+    logger.info(request)
     args = {
         "base_url": request.form['base_url'],
         "api_key": request.form['api_key'],
@@ -33,23 +44,23 @@ def list_grafana_metrics():
 
 @app.route('/prometheus/get', methods=['GET'])
 def get_prometheus_dataseries_num():
+    logger.info(request)
     args = {
         "prom_url": request.args.get('prom_url'),
         "raw": request.args.get('raw')
     }
     try:
         data = prometheus_get(**args)
+        logger.info(data)
     except Exception as e:
         abort(500, e)
 
-    logging.basicConfig()
-    logger = logging.getLogger('waitress')
-    logger.info(data)
     return str(data)
 
 
 @app.route('/prometheus/apply', methods=['POST', ])
 def apply_prometheus():
+    logger.info(request)
     args = {
         "prom_yaml_path": request.form['prom_yaml_path'],
         "metrics_json_path": request.form['metrics_json_path'],
@@ -66,6 +77,7 @@ def apply_prometheus():
 
 @app.route('/prometheus/reload', methods=['POST', ])
 def reload_prometheus():
+    logger.info(request)
     args = {
         "prom_url": request.form['prom_url'],
         "raw": request.form['raw']
@@ -87,12 +99,18 @@ def shutdown_server():
 
 @app.route('/stop', methods=['POST', ])
 def webserver_stop():
+    logger.info(request)
     shutdown_server()
     return 'frigga webserver is shutting down...'
 
 
 def run(port=8083, debug=False):
-    serve(app, host="0.0.0.0", port=port)
+    app.config['TESTING'] = debug
+    logger.info(app)
+    if debug:
+        app.run(host="0.0.0.0", port=port)
+    else:
+        serve(app, host="0.0.0.0", port=port)
 
 
 if __name__ == '__main__':
