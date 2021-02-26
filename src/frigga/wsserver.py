797f08ea85be0c3b6f1efb8a89a1b6d747aaaf44
Meir Gabay <unfor19@gmail.com>
2021-02-27 01:20:33 +0200

    send error msgs to client

--- src/frigga/wsserver.py
+++ src/frigga/wsserver.py
@@ -1,11 +1,16 @@
 import asyncio
 import websockets
 import json
+import logging
 
 from .prometheus import get_total_dataseries as prometheus_get
 from .grafana import get_metrics_list as grafana_list
 from .prometheus import apply_yaml as prometheus_apply
 from .prometheus import reload_prom as prometheus_reload
+
+logging.basicConfig()
+logger = logging.getLogger('websockets')
+logger.setLevel(logging.INFO)
 
 
 async def msg(websocket, path):
@@ -13,24 +18,32 @@ async def msg(websocket, path):
     try:
         received = json.loads(received)
     except Exception:
-        print("simple text")
+        pass
 
     print(f"Requested: {received}")
     message = ""
     if 'action' in received:
-        if received['action'] == "prometheus_get":
-            received.pop('action')
-            message = prometheus_get(**received)
-        elif received['action'] == "grafana_list":
-            received.pop('action')
-            message = grafana_list(**received)
-            message = "Created .metrics.json!"
-        elif received['action'] == "prometheus_apply":
-            received.pop('action')
-            message = prometheus_apply(**received)
-        elif received['action'] == "prometheus_reload":
-            received.pop('action')
-            message = prometheus_reload(**received)
+        try:
+            if received['action'] == "prometheus_get":
+                received.pop('action')
+                message = prometheus_get(**received)
+                message = f">> [LOG] Current number of metrics - {message}"
+            elif received['action'] == "grafana_list":
+                received.pop('action')
+                message = grafana_list(**received)
+                message = f">> [LOG] Successfully created {received['output_file_path']}"  # noqa: 501
+            elif received['action'] == "prometheus_apply":
+                received.pop('action')
+                message = prometheus_apply(**received)
+                message = ">> [LOG] Successfully applied new relabel rules in Prometheus. Reload Prometheus to apply the changes."  # noqa: 501
+            elif received['action'] == "prometheus_reload":
+                received.pop('action')
+                message = prometheus_reload(**received)
+                message = ">> [LOG] Successfully reloaded Prometheus"
+        except Exception as error:
+            logger.error(error)
+            await websocket.close(code=1011, reason=error.__str__()[0:120])
+
     else:
         message = f"Hello {received}!"
     message = json.dumps(message)
