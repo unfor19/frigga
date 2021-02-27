42d96122c47e7bce12809a76ea8c35a10f09314a
Meir Gabay <unfor19@gmail.com>
2021-02-27 18:29:30 +0200

    fix false error

--- kubernetes/validate.sh
+++ kubernetes/validate.sh
@@ -3,4 +3,7 @@ set -e
 set -o pipefail
 kubectl get pods
 POD_PROMETHEUS=$(kubectl get pods --selector=app=prometheus | grep 1/1.*Running | cut -f 1 -d " " 2>/dev/null || true)
-[[ -z ${POD_PROMETHEUS} ]] && echo "The container ${POD_PROMETHEUS} is not running, execute first docker-compose/deploy_stack.sh"
+if [[ -z ${POD_PROMETHEUS} ]]; then
+    echo "The container ${POD_PROMETHEUS} is not running, execute first docker-compose/deploy_stack.sh"
+    exit 1
+fi
